60748324659a7a3afd7b57b60431bf6384b86553
Rockelle Gardner <rockellegardner@gmail.com>
2024-02-22 15:15:32 -0500

    part 2

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add('flex-ctr')
 
-
-
 // var('--main-bg')=document.querySelector(mainEl).style.backgroundColor
+
+// Part 2:
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add("flex-around");
