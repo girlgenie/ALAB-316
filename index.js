134f3130b074d058e4dbc4f4fa1bc818e7db4825
Rockelle Gardner <rockellegardner@gmail.com>
2024-02-22 15:46:32 -0500

    part 3

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+//   const callbackFunction(text,href) => (){}
+// menuLinks.forEach('about')
+
+// Part 3:
+
+const nav = document.querySelector('nav')
+
+    for (var i = 0; i < menuLinks.length; i++) {
+        var link = menuLinks[i];
+        var menuItem = document.createElement('a');
+        menuItem.textContent = link.text;
+        menuItem.href = link.href;
+        nav.appendChild(menuItem);
+    }
+
 const mainEl = document.querySelector('main'); 
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
