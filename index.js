e96dc949205244f329440e4ac23e0f97c284c750
Rockelle Gardner <rockellegardner@gmail.com>
2024-02-22 11:45:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const mainEl = document.querySelector('main'); 
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr')
+
+
+
+// var('--main-bg')=document.querySelector(mainEl).style.backgroundColor
