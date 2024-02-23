d8ad6d190b205baf8a273b8fc9c324be8e13eda6
Rockelle Gardner <rockellegardner@gmail.com>
2024-02-23 13:33:07 -0500

    part 3: Creating the Submenu

--- index.js
+++ index.js
@@ -54,9 +54,25 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add("flex-around");
 
 
-// Part 2-------------------------------------------------------------
+// DOM Manipulation (Part Two) 
+// Part 3: Creating the Submenu ------------------------------------------------------------------
+
+// Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
 const subMenuEl = document.getElementById('sub-menu');
+// Set the height subMenuEl element to be "100%".
 subMenuEl.style.height = "100%"; 
+// Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'; 
+// Add the class of flex-around to the subMenuEl element.
 subMenuEl.classList.add("flex-around"); 
+
+// Set the CSS position property of subMenuEl to the value of absolute.
+document.getElementById("sub-menu").style.position = "absolute"; 
+// Set the CSS top property of subMenuEl to the value of 0.
+document.getElementById("sub-menu").style.top = 0; 
+
+
+
+// Part 4: Adding Menu Interaction----------------------------------------------------------------
+
 console.log(subMenuEl)
